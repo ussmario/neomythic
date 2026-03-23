cc0560ec4da1376670e88721999435723abd083e
Mario <matthew.davies85@gmail.com>
2026-03-22 18:37:35 -0700

    If applied, this commit will: implement the move dictionary

---
+++ src/data/moveDictionary.ts
@@ -0,0 +1,16 @@
+export const MoveDictionary = {
+  Tackle: {
+    power: 40,
+    accuracy: 100,
+    type: "Normal",
+    category: "Physical"
+  },
+  Ember: {
+    power: 40,
+    accuracy: 100,
+    type: "Fire",
+    category: "Special",
+    effect: "Burn",
+    effectChance: 10
+  }
+}
